2b86a339f4345a4f8edea6bc8b967d7cfe2a61a3
Valorando <128958784+Valorando@users.noreply.github.com>
2024-05-13 10:11:48 +0300

    Registring interface and service in DI container, added connection string and SQL requests in appsettings.json, realization interface in service

--- MyStoreAPI/Program.cs
+++ MyStoreAPI/Program.cs
@@ -1,6 +1,19 @@
+using MyStoreAPI.Services;
+using MyStoreAPI.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+var requestsSection = configuration.GetSection("Requests");
+
+var sqlRequests = new Dictionary<string, string>();
+foreach (var request in requestsSection.GetChildren())
+{
+    sqlRequests[request.Key] = request.Value;
+}
 
 // Add services to the container.
+builder.Services.AddScoped<IProductService, ProductService>(provider => new ProductService(connectionString, sqlRequests));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ MyStoreAPI/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using MyStoreAPI.Models;
+using MyStoreAPI.Interfaces;
+
+namespace MyStoreAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly string _connectionString;
+        private readonly Dictionary<string, string> _sqlQueries;
+
+        public ProductService(string connectionString, Dictionary<string, string> sqlQueries)
+        {
+            _connectionString = connectionString;
+            _sqlQueries = sqlQueries;
+        }
+
+        public Task<List<ProductModel>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
