569821a833468d549b4deeb48c25d72e100820a2
Valorando <128958784+Valorando@users.noreply.github.com>
2024-05-15 16:24:31 +0300

    Added service, registring service and interface in Program.cs, added request for post order in appsettings.json

---
+++ MyStoreAPI/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data;
+using MyStoreAPI.Models;
+using MyStoreAPI.Interfaces;
+
+namespace MyStoreAPI.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly string _connectionString;
+        private readonly Dictionary<string, string> _sqlQueries;
+
+        public OrderService(string connectionString, Dictionary<string, string> sqlQueries)
+        {
+            _connectionString = connectionString;
+            _sqlQueries = sqlQueries;
+        }
+
+        public async Task AddOrder(OrderModel orderModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
