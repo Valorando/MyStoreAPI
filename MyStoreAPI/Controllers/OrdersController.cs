7e7423e79a05f49cb68038f2cf087e3fe1a2cc85
Valorando <128958784+Valorando@users.noreply.github.com>
2024-05-16 22:56:22 +0300

    Added logging by Serilog in project

--- MyStoreAPI/Controllers/OrdersController.cs
+++ MyStoreAPI/Controllers/OrdersController.cs
@@ -10,22 +10,34 @@ namespace MyStoreAPI.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly IOrderService _orderService;
+        private readonly ILogger<OrdersController> _logger;
 
-        public OrdersController(IOrderService orderService)
+        public OrdersController(IOrderService orderService, ILogger<OrdersController> logger)
         {
             _orderService = orderService;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] OrderModel orderModel)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
-            }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            await _orderService.AddOrder(orderModel);
-            return CreatedAtAction(nameof(Post), null);
+                _logger.LogInformation($"Client send new order: OrderId={orderModel.OrderId}, OrderDate={orderModel.OrderDate}, FirstName={orderModel.FirstName}, SecondName={orderModel.SecondName}, PhoneNumber={orderModel.PhoneNumber}, ProductName={orderModel.ProductName}, Amount={orderModel.Amount}");
+
+                await _orderService.AddOrder(orderModel);
+
+                return CreatedAtAction(nameof(Post), null);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
         }
     }
 }
